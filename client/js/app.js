7f14d250a0caf0b3dec2b31a6f94d55521e00621
Bram Borggreve <borggreve@gmail.com>
2015-06-04 11:29:14 +0200

    Implementing selecting items for bulk operations

--- client/js/app.js
+++ client/js/app.js
@@ -59,8 +59,55 @@ app.config(['$stateProvider', '$urlRouterProvider',
       .state('app.items', {
         url: '/items',
         templateUrl: 'views/items.html',
-        controller: function (items) {
+        controller: function ($scope, items) {
+          var self = this;
+
+          // The array of items we work with
           this.items = items;
+
+          // The array of selected items
+          this.itemsSelected = [];
+
+          // Watch these items and add the selected ones to a new array
+          $scope.$watch(angular.bind(this, function (items) {
+            return this.items;
+          }), function (newItems) {
+            self.itemsSelected = newItems.filter(function (item) {
+              return item.isSelected;
+            })
+          }, true);
+
+          // The available batch operations
+          this.batchOperations = [];
+
+          // The selected batch operation
+          this.batchOperation = "";
+
+          // The action to run with the batch operations
+          this.applyBatchOperation = function () {
+
+            // Check if there are items selected
+            if (this.itemsSelected.length < 1) {
+              window.alert('Please select one ore more items');
+              return;
+            }
+            console.log('Selected items:', this.itemsSelected);
+
+            // Check if there is a batch operation selected
+            if (this.batchOperation === "") {
+              window.alert('Please pick batch operation');
+              return;
+            }
+            console.log('Selected batch operation:', this.batchOperation);
+
+            this.runBatchOperations(this.batchOperation, this.itemsSelected);
+          };
+
+          // This runs the actual operation on the items
+          this.runBatchOperations = function (action, items) {
+            window.alert('Running', action, 'on', items.length, 'items');
+          };
+
         },
         controllerAs: 'ctrl',
         resolve: {
