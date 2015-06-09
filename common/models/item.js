8ec23ee733e68e9547bc1e94fe412fb3440dc04e
Bram Borggreve <borggreve@gmail.com>
2015-06-10 00:12:38 +0200

    Implement csv export in API

--- common/models/item.js
+++ common/models/item.js
@@ -1,3 +1,36 @@
-module.exports = function(Item) {
+var csv = require('csv');
+
+module.exports = function (Item) {
+
+  Item.export = function (res, cb) {
+    Item.find()
+      .then(function (items) {
+        var content = items.map(function (item) {
+          return {
+            id: item.id,
+            name: item.name,
+            status: item.status
+          }
+        });
+        var options = {
+          header: true,
+          delimiter: ';'
+        };
+        csv.stringify(content, options, function (err, csv) {
+          if (err) cb(err);
+          cb(null, csv);
+        });
+      })
+      .catch(cb);
+  };
+
+
+  Item.afterRemote('export', function (ctx, data, next) {
+    var filename = 'items-export.' + new Date().getTime() + '.csv';
+    ctx.res.attachment(filename);
+    ctx.res.send(data);
+    ctx.res.end();
+  });
+
 
 };
