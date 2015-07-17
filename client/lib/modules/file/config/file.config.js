877b73ec2a56222b67ef2e82ba202caa4707dd51
Bram Borggreve <borggreve@gmail.com>
2015-07-17 03:10:56 +0200

    Add File module

--- client/lib/modules/file/config/file.config.js
+++ client/lib/modules/file/config/file.config.js
@@ -9,14 +9,98 @@ app.config(function ($stateProvider) {
     url: '/list',
     templateUrl: 'lib/modules/file/views/list.html',
     controllerAs: 'ctrl',
-    controller: function (containers) {
+    controller: function (Container, containers) {
       this.containers = containers;
+      this.addContainer = function () {
+        var name = window.prompt('Container name?');
+
+        if (name !== null) {
+          Container.createContainer({
+            name: name
+          }).$promise.then(function(){
+            console.log('name', name);
+          }).catch(function(err){
+            console.log('err', err);
+          });
+        }
+      }
     },
     resolve: {
       containers: function (Container) {
         return Container.getContainers().$promise;
       }
     }
+  }).state('app.files.upload', {
+    url: '/upload/:container',
+    templateUrl: 'lib/modules/file/views/upload.html',
+    resolve: {
+      containers: function (Container) {
+        return Container.getContainers().$promise;
+      },
+      container: function ($stateParams, Container) {
+        return Container.getContainer({
+          container: $stateParams.container
+        }).$promise;
+      },
+      files: function ($stateParams, Container) {
+        return Container.getFiles({
+          container: $stateParams.container
+        }).$promise;
+      }
+    },
+    controller: function ($stateParams, File, containers, container) {
+      this.container = container;
+      this.upload = {
+        container: $stateParams.container,
+        file: {}
+      };
+
+      this.containerList = containers.map(function(container){
+        return {
+          value: container.name,
+          label: container.name
+        }
+      });
+
+      this.schema = {
+        type: 'object',
+        properties: {
+          container: {
+            type: 'string',
+            format: 'uiselect',
+            title: 'Container',
+            items: this.containerList
+          },
+          file: {
+            type: 'file',
+            title: 'File'
+          }
+        }
+      };
+
+      this.form = [{
+        key: 'container'
+      }, {
+        key: 'file',
+        type: 'file'
+      }, {
+        type: 'submit',
+        title: 'Submit'
+      }];
+
+      this.onSubmit = function () {
+        console.log('this.upload', this.upload);
+
+        File.upload(this.upload, function (yay) {
+          console.log('yay', yay);
+        }, function (nay) {
+          console.log('nay', nay);
+        });
+
+      };
+
+    },
+    controllerAs: 'ctrl'
   }).state('app.files.container', {
     url: '/container/:container',
     templateUrl: 'lib/modules/file/views/container.html',
