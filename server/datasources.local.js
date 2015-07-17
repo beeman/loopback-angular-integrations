7d7e3ebfb2977db36950fc700ec4691877db11e3
Bram Borggreve <borggreve@gmail.com>
2015-07-17 03:08:12 +0200

    Add configration for storage providers

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -9,3 +9,35 @@ if (MONGODB_URL) {
     }
   };
 }
+
+var STORAGE_PROVIDER = process.env.STORAGE_PROVIDER || 'filesystem';
+
+if(STORAGE_PROVIDER === 'filesystem') {
+
+  var STORAGE_PATH = process.env.STORAGE_PATH || './storage';
+
+  module.exports = {
+    storage: {
+      name: 'storage',
+      connector: 'loopback-component-storage',
+      provider: STORAGE_PROVIDER ,
+      root: STORAGE_PATH
+    }
+  };
+}
+
+if(STORAGE_PROVIDER === 'amazon') {
+
+  var S3_KEY = process.env.S3_KEY || null;
+  var S3_KEY_ID = process.env.S3_KEY_ID || null;
+
+  module.exports = {
+    storage: {
+      name: 'storage',
+      connector: 'loopback-component-storage',
+      provider: STORAGE_PROVIDER,
+      key: S3_KEY,
+      keyId: S3_KEY_ID
+    }
+  };
+}
